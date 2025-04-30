2b432e9ec327334424e1e8a10ebb1f2d4a74af0b
Hasan Waheed <hasanwaheedbusiness@gmail.com>
2025-05-01 01:54:45 +0500

    main

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 👈 disables type checking during build
+  },
 };
 
 module.exports = nextConfig;
